c28439ae4bf7b55e2fd346cc88e8a84a97700508
Sam Nithish <nithishraj8055@gmail.com>
2026-05-16 16:00:18 +0530

    Rename Footer.css to footer.css

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import "./Footer.css";
+import "./footer.css";
 import logo from "../../assets/logo.png";
 
 const Footer = () => {
